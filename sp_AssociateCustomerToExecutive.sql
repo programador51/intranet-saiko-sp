3ae3b1fe9b9e7a6a4edcfa282bcf79543f8455da
programador51 <gskitcat@gmail.com>
2021-07-26 18:25:49 -0500

    docs: Documentation and rename of sp

--- sp_AssociateCustomerToExecutive.sql
+++ sp_AssociateCustomerToExecutive.sql
@@ -1,4 +1,30 @@
-CREATE PROCEDURE sp_AssociateCustomerToExecutive(
+-- **************************************************************************************************************************************************
+--	STORED PROCEDURE OVERVIEW INFORMATION
+-- **************************************************************************************************************************************************
+-- =============================================
+-- Author:      Jose Luis Perez Olguin
+-- Create date: 07-26-2021
+-- Description: When adds a new customer, associate an executive with that customer created
+-- STORED PROCEDURE NAME:	sp_AddCustomerToExecutive
+-- STORED PROCEDURE OLD NAME: sp_AssociateCustomerToExecutive
+-- **************************************************************************************************************************************************
+-- =============================================
+-- PARAMETERS:
+-- @customerID: Is the id of the customer that is associated with
+-- @executiveID: ID of the executive who attends
+-- @status: 1 active 0 inactive
+-- @createdBy: First name,middle name and lastName1 who created made the relation
+-- ===================================================================================================================================
+-- **************************************************************************************************************************************************
+--	REVISION HISTORY/LOG
+-- **************************************************************************************************************************************************
+--	Date			Programmer					Revision	    Revision Notes			
+-- =================================================================================================
+--	2021-07-22		Iván Díaz   				1.0.0.0			Initial Revision
+--  2021-07-26      Jose Luis Perez             1.0.0.1         Documentation and file name update		
+-- *****************************************************************************************************************************
+
+CREATE PROCEDURE sp_AddCustomerToExecutive(
 
 	@customerID INT,
 	@executiveID INT,
