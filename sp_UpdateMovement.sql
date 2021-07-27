06f5bd3683b849227b3166e29daa7d0027f28b3b
programador51 <gskitcat@gmail.com>
2021-07-27 10:04:05 -0500

    feat:sp_UpdateMovement, creation of query

--- sp_UpdateMovement.sql
+++ sp_UpdateMovement.sql
@@ -5,22 +5,18 @@
 -- Author:      Jose Luis Perez Olguin
 -- Create date: 07-27-2021
 
--- Description: Add a new movement (egress or incoming) associated with a bank account
+-- Description: Update the information of a movement
 
 -- **************************************************************************************************************************************************
 -- =============================================
 -- PARAMETERS:
--- @bank: Bank associated with the bank account
--- @movementType (FK): Type of movement (egress of incoming)
--- @movementTypeNumber (FK): Type of movement (egress of incoming)
--- @reference: Reference of the movement
 -- @concept: Concept of the movement created
--- @amount: Cost of the movement
--- @status: 1 acitve and 0 inactive
--- @createdBy: First name, middlename and lastName1
--- @bankAccount (FK): Bank account it correspond that movement
--- @customerAssociated (FK): Which customer correspond that movement
--- @movementDate: Date it corresponds the movement, format YYYY-MM-DD
+-- @check: Check id of the movement
+-- @id_movement: ID of the movement to edit
+-- @payMethod (FK): Pay method of the movement
+-- @reference: Reference of the movement
+-- @registerDate: Date it corresponds the movement, format YYYY-MM-DD
+-- @modifiedBy: First name, middlename and lastName1
 
 -- ===================================================================================================================================
 -- **************************************************************************************************************************************************
@@ -31,40 +27,28 @@
 --  2021-07-27      Jose Luis Perez             1.0.0.0         Creation of query		
 -- *****************************************************************************************************************************
 
-CREATE PROCEDURE sp_AddMovement(
-    @bank INT
-    @movementType TINYINT,
-    @movementTypeNumber INT,
-    @reference NVARCHAR(30),
+CREATE PROCEDURE sp_UpdateMovement(
     @concept NVARCHAR(256),
-    @amount DECIMAL(14,4),
-    @status TINYINT,
-    @createdBy NVARCHAR(30),
-    @bankAccount INT,
-    @customerAssociated INT,
-    @movementDate NVARCHAR(15)
-
+    @check NVARCHAR(50),
+    @id_movement INT,
+    @payMethod INT,
+    @reference NVARCHAR(30),
+    @registerDate NVARCHAR(15),
+    @modifiedBy NVARCHAR(30)
 )
 
 AS BEGIN
 
-INSERT INTO Movements
-(
-    bankID,movementType,movementTypeNumber,
-    reference,concept,amount,
-    status,createdBy,createdDate,
-    movementDate,
-    bankAccount,customerAssociated
-)
+UPDATE Movements
+    SET
+        concept = @concept,
+        checkNumber = @check,
+        paymentMethod = @payMethod,
+        reference = @reference,
+        movementDate = CONVERT(DATETIME,@registerDate,102),
+        lastUpdatedDate = GETDATE(),
+        lastUpdatedBy = @modifiedBy
 
-VALUES
-
-(
-    @bank,@movementType,@movementTypeNumber,
-    @reference,@concept,@amount,
-    @status,@createdBy,GETDATE(),
-    CONVERT(DATETIME,@movementDate,102),
-    @bankAccount,@customerAssociated
-)
+    WHERE MovementID = @id_movement
 
 END
