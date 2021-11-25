98c8850908d8469415a0249d8847404f754aaf03
Jose Luis Perez Olguin <jperez@saiko.mx>
2021-11-25 11:43:16 -0600

    fix: sp_GetRfc
    Retorna true/false si un rfc ya esta en uso por otro cliente/proveedor en el directorio

--- sp_GetRfc.sql
+++ sp_GetRfc.sql
@@ -9,7 +9,7 @@
 
 -- ===================================================================================================================================
 -- PARAMETERS:
--- @rfc: RFC to find on the system to check if it's repeated
+-- @rfcToCheck: RFC to find on the system to check if it's repeated
 
 -- ===================================================================================================================================
 -- **************************************************************************************************************************************************
@@ -18,21 +18,37 @@
 --	Date			Programmer					Revision	    Revision Notes			
 -- =================================================================================================
 --  15-09-2021     Jose Luis Perez             1.0.0.0         Documentation and query		
+--  25-11-2021     Jose Luis Perez             1.0.0.1         Documentation and query		
 -- *****************************************************************************************************************************
 
 CREATE PROCEDURE sp_GetRfc(
-    @rfc NVARCHAR(30)
+    @rfcToCheck NVARCHAR(30)
 )
 
 AS BEGIN
 
+    -- SELECT
+	--     rfc 
+
+    -- FROM Customers
+
+    -- WHERE
+    --     rfc = @rfc AND
+    --     rfc != 'XAXX010101000';
+
+    DECLARE @rfcFounded NVARCHAR(30);
+
     SELECT
-	    rfc 
+        @rfcFounded = rfc 
 
     FROM Customers
 
     WHERE
-        rfc = @rfc AND
-        rfc != 'XAXX010101000'
+        rfc = @rfcToCheck AND
+        rfc != 'XAXX010101000';
+
+    SET @rfcFounded = ISNULL(DATALENGTH(@rfcFounded) - DATALENGTH(@rfcFounded) + 1,0);
+
+    SELECT CONVERT(BIT,@rfcFounded) AS rfcRepeated;
 
 END
